960cf4e75bbd2c27ef91ca4618eb1b5112f82cd5
huyy <huyb34385@gmail.com>
2025-12-15 22:38:04 +0700

    update

--- app/index.py
+++ app/index.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from app import app
+from app.models import Mon
 
-app = Flask(__name__)
+
 @app.route("/")
 def index():
-    name = "Huy"
-    return render_template("index.html", name=name)
+    ds_mon = Mon.query.all()
+    return render_template("index.html", items=ds_mon)
+
+@app.route("/menu")
+def menu():
+    ds_mon = Mon.query.all()
+    return render_template("layout/menu.html", items=ds_mon)
+
+@app.route("/login")
+def login():
+    return render_template("layout/login.html")
 
 if __name__=="__main__":
     with app.app_context():
